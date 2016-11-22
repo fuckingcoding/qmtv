80e5a29a71e5328a3807ca317ba67f6dca38aa10
minamihoshi <330153254@qq.com>
2016-11-22 20:17:57 +0800

    url

--- app/src/main/java/com/example/acer/myzhibo/config/UrlConfig.java
+++ app/src/main/java/com/example/acer/myzhibo/config/UrlConfig.java
@@ -6,5 +6,21 @@ package com.example.acer.myzhibo.config;
 
 public class UrlConfig {
     public static  final  String BASE_URL ="http://www.quanmin.tv/";
+     public static final  String PATH1= "json/categories/";
+    public  static  final  String PATH2 ="/list.json?11212123&v=2.2.4&os=1&ver=4";
+
+    public static  final String [] URLSTRING = new String[] {
+            "lol","beauty","heartstone","huwai","overwatch","love",
+            "wangzhe","qqfeiche", "yys","tvgame", "cfpc","mobilegame",
+            "fs","au", "blizzard","erciyuan","dota2","zhuangjiafengbao",
+            "dnf","qiuqiu","webgame","chuanqi","war3","fifa","nba2k"
+    };
+
+    public  static  final  String []  ColumnName = new String[]{
+      "英雄联盟","全民星秀","炉石传说","全民户外","守望先锋","颜值控","王者荣耀","QQ飞车",
+            "阴阳师","阴阳师","单机主机","穿越火线","手游专区","街篮专区","劲舞团",
+            "暴雪经典","二次元区","DOTA2","装甲风暴","DNF","球球大作战","网络游戏",
+            "传奇专区","魔兽争霸3","FIFA","NBA2K"
+    };
 
 }
